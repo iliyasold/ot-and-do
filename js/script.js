22d1108ade6f1775da9ac3336d29e958e80b4b60
Ilya Soldatkin <ilyasoldatkin@MacBook-Pro-Ilya.local>
2020-06-24 16:25:29 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Ввод
+// Повесить событие "Клик" на кнопку "Добавить"
+// Введенный текст добавить вместе с <li> в конец списка
+
+// Сменить стиль при клике на задачу на text-decoration: line-through;
+
+// Убрать стиль при повтороном клике на задачу на text-decoration: none;
+
+// document.getElementById("demo").addEventListener("click", myFunction);
+//
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+// }
+
+let newTask = document.getElementsByClassName("main__title").innerHTML;
+
+console.log(newTask);
